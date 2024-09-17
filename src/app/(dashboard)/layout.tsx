4aa7f0203d071eea3986144665bc64bd348aed3d
Surendra Shrestha <xurendra@gmail.com>
2024-09-16 21:49:10 -0400

    menu and navbar changes

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,20 +1,31 @@
 import Link from "next/link";
 import Image from "next/image";
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className="h-screen flex">
-    {/* LEFT */}
-    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
-      <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
-        <Image src="/logo.png" alt="logo" width={32} height={32} />
-        <span className="hidden lg:block">SchoolLama</span>
-      </Link>
+  return (
+    <div className="h-screen flex">
+      {/* LEFT */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">SchoolLama</span>
+        </Link>
+        <Menu />
+      </div>
+      {/* RIGHT */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
     </div>
-    {/* RIGHT */}
-    <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">r</div>
-  </div>;
+  );
 }
